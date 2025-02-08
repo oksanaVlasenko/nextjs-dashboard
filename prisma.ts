39f08b3bf653d5cb4ba949e8e7a37f3426c2be84
Oksana Vlasenko <50576486+oksanaVlasenko@users.noreply.github.com>
2025-02-08 13:14:34 +0200

    fixed deploy errors

--- prisma.ts
+++ prisma.ts
@@ -1,7 +1,17 @@
 import { PrismaClient } from "@prisma/client/edge"
- 
+import { withAccelerate } from '@prisma/extension-accelerate'
+
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
  
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+export const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
  
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+
+// import { PrismaClient } from "@prisma/client";
+
+// const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+
+// export const prisma =
+//   globalForPrisma.prisma || new PrismaClient({});
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
