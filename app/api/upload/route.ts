ed414feafa4557b62c34478f9e7856f8d0395a01
Oksana Vlasenko <50576486+oksanaVlasenko@users.noreply.github.com>
2025-03-01 14:07:24 +0200

    console for testing

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,3 +1,5 @@
+export const config = { runtime: 'experimental-edge' }
+
 import { NextRequest, NextResponse } from "next/server";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import crypto from "crypto";
