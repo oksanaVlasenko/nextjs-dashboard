43dc9efc062d9fa817a3a3a5f2eac36a1b2e007e
Oksana Vlasenko <50576486+oksanaVlasenko@users.noreply.github.com>
2025-03-01 16:54:21 +0200

    console for testing

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import crypto from "crypto";
 
