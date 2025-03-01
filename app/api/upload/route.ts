b765f2ac21025ccd771df4109a245d3f8e94ab4e
Oksana Vlasenko <50576486+oksanaVlasenko@users.noreply.github.com>
2025-03-01 17:20:30 +0200

    console for testing

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,15 +1,15 @@
 import { NextResponse } from "next/server";
-import { S3Client,  } from "@aws-sdk/client-s3";
+//import { S3Client,  } from "@aws-sdk/client-s3";
 // import crypto from "crypto"; PutObjectCommand
 
 
-const s3Client = new S3Client({
-  region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-  },
-});
+// const s3Client = new S3Client({
+//   region: process.env.AWS_REGION,
+//   credentials: {
+//     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+//   },
+// });
 
 export async function POST(req: Request) {
   //console.log('FormData received:', await req.formData());
