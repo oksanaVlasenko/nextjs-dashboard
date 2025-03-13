f3ebf085fec5d0fd82963eda2ac7c7a608eb005f
Oksana Vlasenko <50576486+oksanaVlasenko@users.noreply.github.com>
2025-03-13 23:22:26 +0200

    updated register

--- prisma.ts
+++ prisma.ts
@@ -5,7 +5,7 @@ import { withAccelerate } from '@prisma/extension-accelerate'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
  
-console.log("Using Prisma client in the following environment:", process.env.NODE_ENV);
+console.log("Using Prisma client in the following environment:", process.env.VERCEL_ENV);
 
 export const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
  
