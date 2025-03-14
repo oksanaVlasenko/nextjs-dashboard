ab7dc28e457e3facc57e1629012988b74aecf47f
Oksana Vlasenko <50576486+oksanaVlasenko@users.noreply.github.com>
2025-03-14 22:40:37 +0200

    added logs

--- app/lib/cacheSession.ts
+++ app/lib/cacheSession.ts
@@ -4,5 +4,7 @@ import { cache } from "react";
 export const getSession = cache(async () => {
   const session = await auth();
 
+  console.log(session, ' session cached')
+
   return session || null;
 });
